e33fe815f99024d8ba1115b7bb4142a145e78a5c
kamilyfernanda <143743034+kamilyfernanda@users.noreply.github.com>
2024-03-05 11:31:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const botoes = document.querySelector(".botao")
+
+for (let i= 0;i <botoes.length;i++){
+    botoes[i].onclik = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classlist.remover("ativo");
+        }
+
+        botoes[i].classlist.add("ativo");
+    }
+    console.long();
+}
+    
+
+
+
+
+
+ 
