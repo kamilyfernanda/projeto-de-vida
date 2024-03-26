d15259290f812aec33b5bf15bcab98ba56d3f7dc
kamilyfernanda <143743034+kamilyfernanda@users.noreply.github.com>
2024-03-26 11:24:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const botoes = document.querySelector(".botao")
+const botoes = document.querySelectorAll(".botao")
+const textos = document.querySelectorAll(".aba-conteudo")
 
 for (let i= 0;i <botoes.length;i++){
     botoes[i].onclik = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classlist.remover("ativo");
+            textos[j].classlist.remover("ativo");
         }
 
         botoes[i].classlist.add("ativo");
